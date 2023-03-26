9afddaf12dfb84e69d8468c1740f996d91558064
Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>
2023-03-25 21:50:23 -0700

    init

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Lukas Next.js App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
